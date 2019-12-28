1fd373308d6d0d9d77ef1c2d8fa599d4355768df
SUGURU <the.rain.let.up@gmail.com>
2019-12-29 01:33:44 +0900

    setting wsgi

--- cylan_task_python3/wsgi.py
+++ cylan_task_python3/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise5
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cylan_task_python3.settings.heroku')
 
