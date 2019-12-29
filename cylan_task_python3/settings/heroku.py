9ae0470bd5f140d0da59e43e58c73c51f57fd128
SUGURU <the.rain.let.up@gmail.com>
2019-12-29 17:11:26 +0900

    DATABASE setting Fix

--- cylan_task_python3/settings/heroku.py
+++ cylan_task_python3/settings/heroku.py
@@ -24,7 +24,8 @@ ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWAREDED_PROTO', 'https')
 
