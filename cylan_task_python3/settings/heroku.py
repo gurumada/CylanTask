c8ce0b2d21dcb80d000b6d9caf59ef9a8d7bc762
SUGURU <the.rain.let.up@gmail.com>
2019-12-29 21:57:34 +0900

    delete normepath

--- cylan_task_python3/settings/heroku.py
+++ cylan_task_python3/settings/heroku.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.normpath(os.path.join(BASE_DIR,"assets")),
+    os.path.join(BASE_DIR,"assets"),
 )
 
 
