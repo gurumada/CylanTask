e6cba5d39f3bd3addb074d73f226a66a56a0efe6
SUGURU <the.rain.let.up@gmail.com>
2019-12-31 13:08:54 +0900

    post ordering

--- task/models.py
+++ task/models.py
@@ -6,3 +6,6 @@ class task(models.Model):
     # id = 1, 2, 3, 4,
     content = models.CharField(max_length=140)
     posted_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted_date']
